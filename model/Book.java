789045ee127be372ac2b0f447f60db95b1fb868c
aaron0sprague <aaron0sprague@gmail.com>
2023-02-07 12:58:00 -0500

    book.java

--- model/Book.java
+++ model/Book.java
@@ -1,5 +1,27 @@
 package model;
 
 public class Book {
-    
+    /**
+     * @param args
+     */
+    public static void main(String []args){
+        Integer id = 0;
+        char title 0;
+
+    }
+    public static bookID(int id){
+
+    }   
+    public static bookTitle(char title){
+
+    } 
+    public static author(char auth){
+
+    }
+    public static pubYear(char year){
+
+    }
+    public static status(char stat){
+
+    }
 }
