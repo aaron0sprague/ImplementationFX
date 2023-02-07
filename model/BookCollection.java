63c2496fb4bf200f1a7bc26a3316626c60bdbe30
scoutsk14 <scoutsk14mc@gmail.com>
2023-02-07 13:34:31 -0500

    methods

--- model/BookCollection.java
+++ model/BookCollection.java
@@ -2,6 +2,26 @@ package model;
 
 public class BookCollection {
 
+    static char findBooksOlderThanDate(char year) {
+
+        return year;
+    }
+
+    static char findBooksNewerThanDate(char year) {
+
+        return year;
+    }
+
+    static char findBooksWithTitleLike(char title) {
+
+        return title;
+    }
+
+    static char findBookWithAuthorLike(char author) {
+        return author;
+    }
+
+    
     public BookCollection() {
         bookList = new Vector(); //new Vector<Book>();
     }
