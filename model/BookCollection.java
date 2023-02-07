173513dff9a2956186d673e121654084210780c6
scoutsk14 <scoutsk14mc@gmail.com>
2023-02-07 14:27:20 -0500

    doublecheck

--- model/BookCollection.java
+++ model/BookCollection.java
@@ -21,8 +21,27 @@ public class BookCollection {
         return author;
     }
 
-    
+    public Vector findBooksOlderThanDate(String year) {
+        String query = "SELECT * FROM " + myTableName + " WHERE (pubYear <>> " + year + ") ORDER BY author ASC";
+        return doQuery(query);
+    }
+
+    public Vector findBooksNewerThanDate(String year) {
+        String query = "SELECT * FROM " + myTableName + " WHERE (pubYear > " + year + ") ORDER BY author ASC";
+        return doQuery(query);
+    }
+
+    public Vector findBooksWithTitleLike(String title) {
+        String query = "SELECT * FROM " + myTableName + " WHERE title LIKE '%" + title + "%' ORDER BY author ASC";
+        return doQuery(query);
+    }
+
+    public Vector findBooksWithAuthorLike(String author) {
+        String query = "SELECT * FROM " + myTableName + " WHERE author LIKE '%" + author + "%' ORDER BY author ASC";
+        return doQuery(query);
+    }
+
     public BookCollection() {
         bookList = new Vector(); //new Vector<Book>();
-    }
+    }    
 }
