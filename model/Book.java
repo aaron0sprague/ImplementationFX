64c91f96c06b649c850461aec329d74130664c3a
scoutsk14 <scoutsk14mc@gmail.com>
2023-02-09 13:45:56 -0500

    next

--- model/Book.java
+++ model/Book.java
@@ -1,7 +1,5 @@
-// specify the package
+    // specify the package
 package model;
-
-// system imports
 
 import exception.InvalidPrimaryKeyException;
 import impresario.IView;
@@ -14,22 +12,13 @@ import java.util.Enumeration;
 import java.util.Properties;
 import java.util.Vector;
 
-// project imports
-
-/**
- * The class containing the Book for the ATM application
- */
-//==============================================================
 public class Book extends EntityBase implements IView {
     private static final String myTableName = "Book";
     public String[] fields = {"author", "title", "pubYear", "status"};
+    Object updateStatusMessage = null;
 
-    // GUI Components
-    private String updateStatusMessage = "";
-
-    // constructor for this class
-    public Book(String bookId)
-            throws InvalidPrimaryKeyException {
+    // constructor
+    public Book(String bookId) throws InvalidPrimaryKeyException {
         super(myTableName);
 
         setDependencies();
@@ -63,15 +52,11 @@ public class Book extends EntityBase implements IView {
 
             }
         }
-        // If no book found for this user name, throw an exception
         else {
             throw new InvalidPrimaryKeyException("No book matching id : "
                     + bookId + " found.");
         }
     }
-
-    // Can also be used to create a NEW Book (if the system it is part of
-    // allows for a new book to be set up)
     public Book(Properties bookInfo) {
         super(myTableName);
 
@@ -87,7 +72,6 @@ public class Book extends EntityBase implements IView {
             }
         }
     }
-
     public Book() {
         super(myTableName);
         setDependencies();
@@ -102,6 +86,7 @@ public class Book extends EntityBase implements IView {
     }
 
     public Object getState(String key) {
+  
         if (key.equals("UpdateStatusMessage"))
             return updateStatusMessage;
 
@@ -116,18 +101,12 @@ public class Book extends EntityBase implements IView {
 
         myRegistry.updateSubscribers(key, this);
     }
-
-    /**
-     * Called via the IView relationship
-     */
     public void updateState(String key, Object value) {
         stateChangeRequest(key, value);
     }
-
     public void update() {
         updateStateInDatabase();
     }
-
     private void updateStateInDatabase() {
         try {
             if (persistentState.getProperty("bookId") != null) {
@@ -154,7 +133,6 @@ public class Book extends EntityBase implements IView {
     {
         // create our new view
         View newView = ViewFactory.createView("BookView", this);
-        Scene newScene = new Scene(newView);
 
         // make the view visible by installing it into the frame
         swapToView(newScene);
@@ -173,10 +151,6 @@ public class Book extends EntityBase implements IView {
         }
         updateStateInDatabase();
     }
-
-    /**
-     * This method is needed solely to enable the Book information to be displayable in a table
-     */
     public Vector<String> getEntryListView() {
         Vector<String> v = new Vector<>();
 
