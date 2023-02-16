4d6be40075e8acdb4cde2eefaeab71244025df48
scoutsk14 <scoutsk14mc@gmail.com>
2023-02-16 12:53:10 -0500

    compile error

--- model/EntityBase.java
+++ model/EntityBase.java
@@ -138,14 +138,14 @@ public abstract class EntityBase extends Persistable
 				"Missing view for display ", Event.ERROR);
 			return;
 		}
- */
-		/* myStage.setScene(otherView);
+ 
+		 myStage.setScene(otherView);
 		myStage.sizeToScene();
 			
 		//Place in center
 		WindowPosition.placeCenter(myStage);
-		 */
+		 
     }
-
+	*/
 }
 
