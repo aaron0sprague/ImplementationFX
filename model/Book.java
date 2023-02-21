e78702ad781ec4aceb550838ea0dda09320f2368
aaron0sprague <aaron0sprague@gmail.com>
2023-02-21 10:23:34 -0500

    file add

--- model/Book.java
+++ model/Book.java
@@ -1,137 +1,342 @@
+// package model;
+
+// import exception.InvalidPrimaryKeyException;
+// import impresario.IView;
+// // import javafx.scene.Scene;
+// // import userinterface.View;
+// // import userinterface.ViewFactory;
+
+
+// import java.sql.SQLException;
+// import java.util.Enumeration;
+// import java.util.Properties;
+// import java.util.Vector;
+
+// import javax.swing.text.View;
+// import javax.swing.text.ViewFactory;
+
+// public class Book extends EntityBase implements IView {
+//     private static final String myTableName = "Book";
+//     public String[] fields = {"author", "title", "pubYear", "status"};
+//     //GUI
+//     private String updateStatusMessage = "";
+//     // constructor
+//     public Book(String bookId)
+//             throws InvalidPrimaryKeyException {
+//         super(myTableName);
+//         setDependencies();
+//         String query = String.format("SELECT * FROM %s WHERE (bookId = %s)",
+//                 myTableName, bookId);
+//         Vector<Properties> allDataRetrieved = getSelectQueryResult(query);
+//         // Book exists
+//         if (allDataRetrieved != null){
+//             int size = allDataRetrieved.size();
+//             if (size != 1){
+//                 throw new InvalidPrimaryKeyException("Multiple books matching id : "+bookId+" found.");
+//             } else{
+//                 Properties retrievedBookData = allDataRetrieved.elementAt(0);
+//                 persistentState = new Properties();
+//                 Enumeration allKeys = retrievedBookData.propertyNames();
+//                 while (allKeys.hasMoreElements()){
+//                     String nextKey = (String) allKeys.nextElement();
+//                     String nextValue = retrievedBookData.getProperty(nextKey);
+//                     if (nextValue != null){
+//                         persistentState.setProperty(nextKey, nextValue);
+//                     }
+//                 }
+//             }
+//         }
+//         // If no book found, throw exception
+//         else{
+//             throw new InvalidPrimaryKeyException("No book matching id : "+bookId+" found.");
+//         }
+//     }
+//     // used to create new book
+//     public Book(Properties bookInfo) {
+//         super(myTableName);
+//         setDependencies();
+//         persistentState = new Properties();
+//         Enumeration allKeys = bookInfo.propertyNames();
+//         while (allKeys.hasMoreElements()) {
+//             String nextKey = (String) allKeys.nextElement();
+//             String nextValue = bookInfo.getProperty(nextKey);
+//             if (nextValue != null){
+//                 persistentState.setProperty(nextKey, nextValue);
+//             }
+//         }
+//     }
+//     public Book(){
+//         super(myTableName);
+//         setDependencies();
+//         persistentState = new Properties();
+//     }
+//     private void setDependencies(){
+//         Properties dependencies = new Properties();
+//         dependencies.setProperty("CancelAddBook", "CancelTransaction");
+//         myRegistry.setDependencies(dependencies);
+//     }
+//     public Object getState(String key){
+//         if (key.equals("UpdateStatusMessage")){
+//             return updateStatusMessage;
+//         }
+//         return persistentState.getProperty(key);
+//     }
+//     public void stateChangeRequest(String key, Object value){
+//         if (key.equals("InsertBook")){
+//             processNewBook((Properties) value);
+//         }
+//         myRegistry.updateSubscribers(key, this);
+//     }
+//     public void updateState(String key, Object value){
+//         stateChangeRequest(key, value);
+//     }
+//     public void update(){
+//         updateStateInDatabase();
+//     }
+//     private void updateStateInDatabase(){
+//         try {
+//             if (persistentState.getProperty("bookId") != null){
+//                 Properties whereClause = new Properties();
+//                 whereClause.setProperty("bookId",persistentState.getProperty("bookId"));
+//                 updatePersistentState(mySchema, persistentState, whereClause);
+//                 updateStatusMessage = "Book data for book ID : "+persistentState.getProperty("bookId")+" updated successfully in database!";
+//             } else{
+//                 Integer bookId = insertAutoIncrementalPersistentState(mySchema, persistentState);
+//                 persistentState.setProperty("bookId", "" + bookId);
+//                 updateStatusMessage = "Book data for new book : "+persistentState.getProperty("bookId")+"installed successfully in database!";
+//             }
+//         } catch (SQLException ex){
+//             updateStatusMessage = "Error inserting book data into database!";
+//         }
+//     }
+//     /**
+//      * 
+//      */
+//     protected void createAndShowBookView(){
+//         final View newView = ViewFactory.createView("BookView", this);
+//         Scene newScene = new Scene(newView);
+//         swapToView(newScene);
+//     }
+//     private void processNewBook(Properties bookInfo){
+//         persistentState = new Properties();
+//         Enumeration allKeys = bookInfo.propertyNames();
+//         while (allKeys.hasMoreElements()){
+//             String nextKey = (String) allKeys.nextElement();
+//             String nextValue = bookInfo.getProperty(nextKey);
+//             if (nextValue != null){
+//                 persistentState.setProperty(nextKey, nextValue);
+//             }
+//         }
+//         updateStateInDatabase();
+//     }
+//     public Vector<String> getEntryListView() {
+//         Vector<String> v = new Vector<>();
+//         v.addElement(persistentState.getProperty("bookId"));
+//         v.addElement(persistentState.getProperty("author"));
+//         v.addElement(persistentState.getProperty("title"));
+//         v.addElement(persistentState.getProperty("pubYear"));
+//         v.addElement(persistentState.getProperty("status"));
+//         return v;
+//     }
+//     protected void initializeSchema(String tableName) {
+//         if (mySchema == null) {
+//             mySchema = getSchemaInfo(tableName);
+//         }
+//     }
+// }
+
+// specify the package
 package model;
-
-import exception.InvalidPrimaryKeyException;
-import impresario.IView;
-import javafx.scene.Scene;
-import userinterface.View;
-import userinterface.ViewFactory;
-
 
 import java.sql.SQLException;
 import java.util.Enumeration;
 import java.util.Properties;
 import java.util.Vector;
 
+import javax.swing.text.View;
+import javax.swing.text.ViewFactory;
+
+// system imports
+
+import exception.InvalidPrimaryKeyException;
+import impresario.IView;
+// import javafx.scene.Scene;
+
+// project imports
+
+/**
+ * The class containing the Book for the ATM application
+ */
+//==============================================================
 public class Book extends EntityBase implements IView {
     private static final String myTableName = "Book";
     public String[] fields = {"author", "title", "pubYear", "status"};
-    //GUI
+
+    // GUI Components
     private String updateStatusMessage = "";
-    // constructor
+
+    // constructor for this class
     public Book(String bookId)
             throws InvalidPrimaryKeyException {
         super(myTableName);
+
         setDependencies();
         String query = String.format("SELECT * FROM %s WHERE (bookId = %s)",
                 myTableName, bookId);
+
         Vector<Properties> allDataRetrieved = getSelectQueryResult(query);
-        // Book exists
-        if (allDataRetrieved != null){
+
+        // Book must exist
+        if (allDataRetrieved != null) {
             int size = allDataRetrieved.size();
-            if (size != 1){
-                throw new InvalidPrimaryKeyException("Multiple books matching id : "+bookId+" found.");
-            } else{
+
+            // There should be EXACTLY one book. Else error
+            if (size != 1) {
+                throw new InvalidPrimaryKeyException("Multiple books matching id : "
+                        + bookId + " found.");
+            } else {
+                // copy all the retrieved data into persistent state
                 Properties retrievedBookData = allDataRetrieved.elementAt(0);
                 persistentState = new Properties();
+
                 Enumeration allKeys = retrievedBookData.propertyNames();
-                while (allKeys.hasMoreElements()){
+                while (allKeys.hasMoreElements()) {
                     String nextKey = (String) allKeys.nextElement();
                     String nextValue = retrievedBookData.getProperty(nextKey);
-                    if (nextValue != null){
+
+                    if (nextValue != null) {
                         persistentState.setProperty(nextKey, nextValue);
                     }
                 }
+
             }
         }
-        // If no book found, throw exception
-        else{
-            throw new InvalidPrimaryKeyException("No book matching id : "+bookId+" found.");
+        // If no book found for this user name, throw an exception
+        else {
+            throw new InvalidPrimaryKeyException("No book matching id : "
+                    + bookId + " found.");
         }
     }
-    // used to create new book
+
+    // Can also be used to create a NEW Book (if the system it is part of
+    // allows for a new book to be set up)
     public Book(Properties bookInfo) {
         super(myTableName);
+
         setDependencies();
         persistentState = new Properties();
         Enumeration allKeys = bookInfo.propertyNames();
         while (allKeys.hasMoreElements()) {
             String nextKey = (String) allKeys.nextElement();
             String nextValue = bookInfo.getProperty(nextKey);
-            if (nextValue != null){
+
+            if (nextValue != null) {
                 persistentState.setProperty(nextKey, nextValue);
             }
         }
     }
-    public Book(){
+
+    public Book() {
         super(myTableName);
         setDependencies();
         persistentState = new Properties();
     }
-    private void setDependencies(){
+
+    private void setDependencies() {
         Properties dependencies = new Properties();
         dependencies.setProperty("CancelAddBook", "CancelTransaction");
+
         myRegistry.setDependencies(dependencies);
     }
-    public Object getState(String key){
-        if (key.equals("UpdateStatusMessage")){
+
+    public Object getState(String key) {
+        if (key.equals("UpdateStatusMessage"))
             return updateStatusMessage;
-        }
+
         return persistentState.getProperty(key);
     }
-    public void stateChangeRequest(String key, Object value){
-        if (key.equals("InsertBook")){
+
+    public void stateChangeRequest(String key, Object value) {
+
+        if (key.equals("InsertBook")) {
             processNewBook((Properties) value);
         }
+
         myRegistry.updateSubscribers(key, this);
     }
-    public void updateState(String key, Object value){
+
+    /**
+     * Called via the IView relationship
+     */
+    public void updateState(String key, Object value) {
         stateChangeRequest(key, value);
     }
-    public void update(){
+
+    public void update() {
         updateStateInDatabase();
     }
-    private void updateStateInDatabase(){
+
+    private void updateStateInDatabase() {
         try {
-            if (persistentState.getProperty("bookId") != null){
+            if (persistentState.getProperty("bookId") != null) {
                 Properties whereClause = new Properties();
-                whereClause.setProperty("bookId",persistentState.getProperty("bookId"));
+                whereClause.setProperty("bookId",
+                        persistentState.getProperty("bookId"));
                 updatePersistentState(mySchema, persistentState, whereClause);
-                updateStatusMessage = "Book data for book ID : "+persistentState.getProperty("bookId")+" updated successfully in database!";
-            } else{
-                Integer bookId = insertAutoIncrementalPersistentState(mySchema, persistentState);
+                updateStatusMessage = "Book data for book ID : "
+                        + persistentState.getProperty("bookId")
+                        + " updated successfully in database!";
+            } else {
+                Integer bookId =
+                        insertAutoIncrementalPersistentState(mySchema, persistentState);
                 persistentState.setProperty("bookId", "" + bookId);
-                updateStatusMessage = "Book data for new book : "+persistentState.getProperty("bookId")+"installed successfully in database!";
+                updateStatusMessage = "Book data for new book : "
+                        + persistentState.getProperty("bookId")
+                        + "installed successfully in database!";
             }
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             updateStatusMessage = "Error inserting book data into database!";
         }
     }
-    protected void createAndShowBookView(){
+    protected void createAndShowBookView()
+    {
+        // create our new view
         View newView = ViewFactory.createView("BookView", this);
         Scene newScene = new Scene(newView);
+
+        // make the view visible by installing it into the frame
         swapToView(newScene);
     }
-    private void processNewBook(Properties bookInfo){
+
+    private void processNewBook(Properties bookInfo) {
         persistentState = new Properties();
         Enumeration allKeys = bookInfo.propertyNames();
-        while (allKeys.hasMoreElements()){
+        while (allKeys.hasMoreElements()) {
             String nextKey = (String) allKeys.nextElement();
             String nextValue = bookInfo.getProperty(nextKey);
-            if (nextValue != null){
+
+            if (nextValue != null) {
                 persistentState.setProperty(nextKey, nextValue);
             }
         }
         updateStateInDatabase();
     }
+
+    /**
+     * This method is needed solely to enable the Book information to be displayable in a table
+     */
     public Vector<String> getEntryListView() {
         Vector<String> v = new Vector<>();
+
         v.addElement(persistentState.getProperty("bookId"));
         v.addElement(persistentState.getProperty("author"));
         v.addElement(persistentState.getProperty("title"));
         v.addElement(persistentState.getProperty("pubYear"));
         v.addElement(persistentState.getProperty("status"));
+
         return v;
     }
+
     protected void initializeSchema(String tableName) {
         if (mySchema == null) {
             mySchema = getSchemaInfo(tableName);
